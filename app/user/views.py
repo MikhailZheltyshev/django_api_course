3da87cd7aa567ef71368fb7b1ccff1e470fb97ca
Mike Zheltyshev <mihalich93@bk.ru>
2019-10-31 20:09:49 +0300

    added create users api

---
+++ app/user/views.py
@@ -0,0 +1,8 @@
+from rest_framework import generics
+
+from .serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in system"""
+    serializer_class = UserSerializer
